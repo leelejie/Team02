d2f84d43ca10f6be09f9c654c888935ae1db7dc9
xavierxiejunjie <S10242383@connect.np.edu.sg>
2023-01-31 09:55:03 +0800

    cash_on_hand.py

--- cash_on_hand.py
+++ cash_on_hand.py
@@ -1,6 +1,10 @@
 # integrated group project
 from pathlib import Path
 import csv
-fp = Path.cwd()/"cash on hand.csv"
+fp = Path.cwd()/"Cash on Hand.csv"
+
+# def cash_counter():
+#      cash_on_hand = []
 
 
+#       for num in 
