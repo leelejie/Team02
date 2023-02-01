c7376756eb8a08e96de7c7c824cf2318ce151f26
leelejie <lejie876@gmai.com>
2023-02-01 19:27:22 +0800

    update

--- profit_loss.py
+++ profit_loss.py
@@ -11,14 +11,26 @@ with fp.open(mode="r", encoding="UTF-8", newline="") as file:
         if the net profit on the current day is lower than the previous day
         - 
         """
+        #creating prevprofit variable to store the previous net profit value
+        #creating count variable to track the different rows in the data
         prevprofit = 0
         count = 0
+
+        #for loop to go through every row of data in the list of data
         for row in reader:
+
+            #use of conditionals where code will run if current day net profit is lower than previous day
             if int(prevprofit) > int(row[4]) and count>0:
+
+                #calculate the difference in net profit
                 diff = int(prevprofit) - int(row[4])
-                return(f"[PROFIT DEFICIT] DAY: {row[0]}, AMOUNT: {diff}")
+                noprofitdeficit = False
+
+                #prints the output desired if the if statement occurs
+                print(f"[PROFIT DEFICIT] DAY: {row[0]}, AMOUNT: {diff}")
+            
             prevprofit = row[4]
             count+=1
-        else:
-            return(f"[NET PROFIT SURPLUS] NET PROFIT ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY")
-    print(profit_loss())    
+        if noprofitdeficit == True:
+            print("no result")
+    profit_loss()
