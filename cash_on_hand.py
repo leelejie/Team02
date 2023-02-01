ac3e70d3c582ea39e726b2729c517de70479cad6
xavierxiejunjie <S10242383@connect.np.edu.sg>
2023-02-01 22:19:22 +0800

    cash_on_hand.py

--- cash_on_hand.py
+++ cash_on_hand.py
@@ -13,27 +13,29 @@ with fp.open(mode="r", encoding="UTF-8", newline="") as file:
         This function identifies the days where the previous day is higher than the current day
         and computes the difference 
         """
+        
         prev_cash = 0
         count = 0
-        #use of loops to go through every item in the list
-       
-        
-        for num in reader: 
-        #if statement so that if the previous day is higher than the current day in the statement it will commit the code below 
+
+        for num in reader:
+        #if statement so that if the previous day is higher than the current day in the statement it will commit the code below
             if int(prev_cash)>int(num[1]) and count>0:
-                
-                #calculating the difference in the cash 
+               
+                #calculating the difference in the cash
                 diff = int(prev_cash) - int(num[1])
+                nocashdeficit = False
 
                 #return back the day and the difference of the cash deficit
-                return f"[CASH DEFICIT] DAY: {num[0]}, AMOUNT: {diff}"
+                return (f"[CASH DEFICIT] DAY: {num[0]}, AMOUNT: {diff}")
 
             prev_cash = num[1]
             count+=1
             
-
             #if the previous day is lower than the current day it will commit the code below
-        else:
+        if nocashdeficit == True:
             return f"[CASH SURPLUS] CASH ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY"
 
+
     print(cash_loss())
+
+
