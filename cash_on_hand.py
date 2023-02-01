20b7485bde54c1b48be10bee1e10e98680ce45bb
xavierxiejunjie <S10242383@connect.np.edu.sg>
2023-02-02 00:45:47 +0800

    cash_on_hand.py

--- cash_on_hand.py
+++ cash_on_hand.py
@@ -13,20 +13,40 @@ with fp.open(mode="r", encoding="UTF-8", newline="") as file:
         This function identifies the days where the previous day is higher than the current day
         and computes the difference 
         """
-        coh_value=[]
-        coh_dayandvalue=[]
-        dayofdeficit=[]
-        index=1
-        for value in reader: 
-            coh_dayandvalue.append(value)
-            coh_value.append(value[1])
-        length=len(coh_value)
-        
-        while index<length:
-            if float(coh_value[index-1])>float(coh_value[index]):
-                print(coh_value[index])
-            index+=1
+        prev_cash = 0
+        count = 0
+        nocashdeficit = True
+
+        #for loop to go through every data
+        for num in reader:
+
+            #The code will execute when the below condition is met which is that the previous day has higher cash than current day
+            if int(prev_cash) > int(num[1]) and count>0:
+
+                #assigning nocashdeficit to be false as a result of if statement
+                nocashdeficit = False
+
+                #calculating the difference in cash 
+                diff = int(prev_cash) - int(num[1])
+
+                #prints the output the if statement occurs
+                print(f"[CASH DEFICIT] DAY: {num[0]}, AMOUNT: USD{diff}")
             
+            #reassign a new value to the variable to store the updated previous data
+            #count to track the row number in the dataset so as to skip the first row 
+            prev_cash = num[1]
+            count+=1
+        
+            #if statement for when the current values are higher than the previous value and will execute the code below
+        if nocashdeficit == True:            
+            #prints the output if statement occurs
+            print("[CASH SURPLUS] CASH ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY")
+    
+    #executes the function
+    cash_loss()
+            
+        
+        
 
 
 
