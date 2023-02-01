ae8deab9eceaa414a8b881cdf1ed23979043c323
leelejie <lejie876@gmai.com>
2023-02-01 22:12:03 +0800

    updated

--- profit_loss.py
+++ profit_loss.py
@@ -1,15 +1,17 @@
 from pathlib import Path
 import csv
+#creating a file to csv file
 fp = Path.cwd()/"CSV_reports"/"Profit and Loss.csv"
 with fp.open(mode="r", encoding="UTF-8", newline="") as file:
     reader = csv.reader(file)
-    next(reader)
+    next(reader) #skip header
     
     def profit_loss():
         """
         - This function computes the difference in the net profit column
         if the net profit on the current day is lower than the previous day
-        - 
+        - Otherwise it returns net profit surplus if all the net profit for the
+        current day is higher than the previous day
         """
         #creating prevprofit variable to store the previous net profit value
         #creating count variable to track the different rows in the data
@@ -21,9 +23,10 @@ with fp.open(mode="r", encoding="UTF-8", newline="") as file:
         #for loop to go through every row of data in the list of data
         for row in reader:
 
-            #use of conditionals where code will run if current day net profit is lower than previous day
+            #use of conditionals where code will be committed if current day net profit is lower than previous day
             if int(prevprofit) > int(row[4]) and count>0:
 
+                #assigning noprofitdeficit to be false as a result of if statement
                 noprofitdeficit = False
 
                 #calculating the difference in net profit
@@ -36,8 +39,10 @@ with fp.open(mode="r", encoding="UTF-8", newline="") as file:
             #count to track the row number in the dataset so as to skip the first row 
             prevprofit = row[4]
             count+=1
+        
         #if statement for when there is no output values meaning all the current values are higher than the previous value
         if noprofitdeficit == True:
+            
             #prints the output desired if the if statement occurs
             print("[NET PROFIT SURPLUS] NET PROFIT ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY")
     
