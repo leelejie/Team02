52bf8506cc78ef1a91f769d72f7c647f1e80d1ba
xavierxiejunjie <S10242383@connect.np.edu.sg>
2023-02-01 14:34:20 +0800

    cash_on_hand.py

--- cash_on_hand.py
+++ cash_on_hand.py
@@ -8,28 +8,31 @@ with fp.open(mode="r", encoding="UTF-8", newline="") as file:
     reader = csv.reader(file)
     next(reader)
     
-    def cash_loss(option):
-        
+    def cash_loss():
+        """
+        this function identifies the days where the previous day is higher than the current day
+        and computes the difference 
+        """
         prev_cash = 0
         count = 0
         #use of loops to go through every item in the list
-        if option == "cash":
+        
+        
+        for num in reader:
+        #if statement so that if the previous day is higher than the current day in the statement it will commit the code below 
+            if int(prev_cash)>int(num[1]) and count>0:
+                
+                #calculating the difference in the cash 
+                diff = int(prev_cash) - int(num[1])
 
-            for num in reader:
-            #if statement so that if the previous day is higher than the current day in the statement it will commit the code below 
-                if int(prev_cash)>int(num[1]) and count>0:
+                #return back the day and the difference of the cash deficit
+                return f"[CASH DEFICIT] DAY: {num[0]}, AMOUNT: {diff_sg}"
 
-            #calculating the difference in the cash and use of abs to remove the negative sign to display the difference in the return statement
-                   diff = abs(int(num[1]) - int(prev_cash))
-
-            #return back the day and the difference of the cash deficit
-                   return f"[CASH DEFICIT] DAY: {num[0]}, AMOUNT: {diff}"
-
-                prev_cash = num[1]
-                count+=1
+            prev_cash = num[1]
+            count+=1
 
             #if the previous day is lower than the current day it will commit the code below
-            else:
-                return f"[CASH SURPLUS] CASH ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY"
+        else:
+            return f"[CASH SURPLUS] CASH ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY"
 
-    print(cash_loss("cash"))
+    print(cash_loss())
