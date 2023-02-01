519194a72c303e7d986939a14c6c50b3315cb8ef
hiimsamantha <samanthakhoolx@gmail.com>
2023-02-02 01:12:47 +0800

    overheads.py

--- overheads.py
+++ overheads.py
@@ -1,36 +1,40 @@
 from pathlib import Path
 import csv
+#crete file path to csv file 
 fp = Path.cwd()/"CSV_reports"/"Overheads.csv"
+#read csv file to append data from csv- overhead category and value
 with fp.open(mode = "r",encoding = "UTF-8", newline="") as file:
     reader= csv.reader(file)
     #skip header
     next(reader)
-
+    #define function
     def overhead():
+        #docstrings to describe function
         """
-        This function returns the highest overhead category
+        - This function calculates and returns the highest overhead 
+        category and its percentage
+        - There are no parameters required
         """
+        #create empty lists and strings to store value later on
         overhead_value=[]
         overhead_category=[]
         overhead_valueandcategory=[]
+        highestoverhead_category=""
+        #create for loop to append overhead category and value to respective lists
         for line in reader:
             overhead_valueandcategory.append(line)
             overhead_value.append(float(line[1]))
             overhead_category.append(line[0])
+        #use max function to find largest value in the list storing overhead values
         largest_value=max(overhead_value)
-        highest_overheadcategory=[]
-        if largest_value==float(overhead_valueandcategory[0][1]):
-            highest_overheadcategory=overhead_valueandcategory[0][0]
-        elif largest_value==float(overhead_valueandcategory[1][1]):
-            highest_overheadcategory=overhead_valueandcategory[1][0]
-        elif largest_value==float(overhead_valueandcategory[2][1]):
-            highest_overheadcategory=overhead_valueandcategory[2][0]
-        elif largest_value==float(overhead_valueandcategory[3][1]):
-            highest_overheadcategory=overhead_valueandcategory[3][0]
-        elif largest_value==float(overhead_valueandcategory[4][1]):
-           highest_overheadcategory=overhead_valueandcategory[4][0]
-        elif largest_value==float(overhead_valueandcategory[5][1]):
-           highest_overheadcategory=overhead_valueandcategory[5][0] 
-        return f"[HIGHEST OVERHEADS] {highest_overheadcategory}:"
-    print(overhead())   
-file_path= Path(r"C:\PFB\summary_report.txt") 
+        #create for loop to access sublists in the nested list (overhead_valueandcategory)
+        for sublist in overhead_valueandcategory:
+            #create if statement to check if each overhead value from the sublist matches the 
+            #max overhead value to find its respective category
+            if float(sublist[1])==largest_value:
+                highestoverhead_category=sublist[0]
+                #statement returned as output
+                return (f"[HIGHEST OVERHEADS] {highestoverhead_category}: {largest_value}%")
+    print(overhead())
+
+        
