7944f6f97379b0e2bd27718a20349916c18d77fb
hiimsamantha <samanthakhoolx@gmail.com>
2023-02-02 21:07:58 +0800

    cash_on_hand.py

--- cash_on_hand.py
+++ cash_on_hand.py
@@ -1,68 +1,48 @@
 from pathlib import Path
 import csv
+
+
 fp = Path.cwd()/"CSV_reports"/"Cash on Hand.csv"
 with fp.open(mode="r", encoding="UTF-8", newline="") as file:
     reader = csv.reader(file)
     next(reader)
-    
-    def cash_loss():
+    def coh_function():
+        
         """
-        This function identifies the days where cash on hand for the previous day is higher than the current day
-        and computes the difference between them
-        No parameters required
+        This function...
         """
-        prev_cash = 0
-        count = 0
-        nocashdeficit = True
+        coh_list=[]
+        day_list=[]
+        coh_day_list=[]
+        listdeficit_coh=[]
+        listdeficit_day=[]
+        listdeficit_diff=[]
+        statement=[]
+        for values in reader:
+            coh_list.append(values[1])
+            day_list.append(values[0])
+            coh_day_list.append(values)
+        length=len(day_list)
+        for index, (day,coh) in enumerate(coh_day_list):
+            if 0<index<length:
+                prev_coh=float(coh_list[index-1])
+                if float(coh)<prev_coh:
+                    listdeficit_day.append(day)
+                    listdeficit_coh.append(float(coh))
+                    listdeficit_diff.append(float(prev_coh)-float(coh))
+        lengthd=len(listdeficit_day)
+        if listdeficit_day!=[]:
 
-        #for loop to go through every data
-        for num in reader:
+            for count in range(lengthd):            
+                d_day=listdeficit_day[count]
+                d_dif=listdeficit_diff[count]
+                print(f"[CASH DEFICIT] DAY: {float(d_day)}, AMOUNT: USD{d_dif}")
+                     
 
-           
-
-            if int(prev_cash) > int(num[1]) and count>0:
-
-                #assigning nocashdeficit to be false as a result of if statement
-                nocashdeficit = False
-
-                #calculating the difference in cash 
-                diff = int(prev_cash) - int(num[1])
-
-                #prints the output if the if statement occurs
-                print(f"[CASH DEFICIT] DAY: {num[0]}, AMOUNT: USD{diff}")
-            
-            #reassign a new value to the variable to store the updated previous data
-            #count to track the row number in the dataset so as to skip the first row 
-            prev_cash = num[1]
-            count+=1
-
-         #The code will execute when the below condition is met which is that the previous day has higher cash than current day
-        if nocashdeficit == True:            
-            #prints the output if statement occurs
-            print("[CASH SURPLUS] CASH ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY")
-            #if statement for when the current values are higher than the previous value and will execute the code below
-       
-    
-    #executes the function
-    cash_loss()
-            
+        else:
+            print(f"[CASH SURPLUS] CASH ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY")
+    coh_function()
         
         
 
-
-
-
-
-
-
-
-
-
-
-
-
         
-
-    print(cash_loss())
-
-
