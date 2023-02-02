d43dcc2ad0e0283835ea8cc2a08ba4be2fe81127
hiimsamantha <samanthakhoolx@gmail.com>
2023-02-02 23:53:30 +0800

    main.py

--- main.py
+++ main.py
@@ -1,27 +1,26 @@
 
-#to import the independant functions into this main function
+
+#to import the independent functions into this main function
 import cash_on_hand,overheads, profit_loss
 
 #to call the different functions into the main function 
 def main():
 
         cash_on_hand.coh_function()
-        overheads.overhead_function()
+        overheads.overheads_function()
         profit_loss.profitloss_function()
 
 output=main()
-from pathlib import Path
 
 #This Function identifies as an iterator for all searhces of the files and folders in the directory 
 #consisting of cash on hand, overheads and profit loss. 
 
 # create a path object for contacts.txt
-file_path= Path(r"C:\Team02\summary_report.txt")  
-print(file_path.exists())
+from pathlib import Path
+file_path= Path(r"C:\PFB\summary_report.txt")
 #opening file with 'with' and 'open'
 #w passed to the mode parameter so i can write a text file
 #allows it to write the function output into a text file
 with file_path.open(mode = "w") as file:
     #write() method to write data into plain text file
     file.write(f"{output}")
-
--- testing.py
+++
@@ -1,3 +0,0 @@
-from pathlib import Path
-file_path= Path(r"C:\Team02\summary_report.txt") 
-print(file_path.exists())
